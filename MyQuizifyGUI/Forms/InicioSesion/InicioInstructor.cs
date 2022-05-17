0f6095b7fc617c9d31b8f2c60114e321c228c17d
Microhard1 <hardhe21@gmail.com>
2022-05-17 10:20:16 +0200

    Cambios navbar

--- MyQuizifyGUI/Forms/InicioSesion/InicioInstructor.cs
+++ MyQuizifyGUI/Forms/InicioSesion/InicioInstructor.cs
@@ -35,21 +35,59 @@ namespace MyQuizifyGUI.Forms.InicioSesion
         }
         private void button1_Click(object sender, EventArgs e)
         {
+            cambiarColorBotones(botonGestionQuiz.Name);
             abrirFormHijo(new QuizesActivos()) ;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            cambiarColorBotones(button3.Name);
             abrirFormHijo(new CursosInstructor());
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            cambiarColorBotones(button4.Name);
             abrirFormHijo(new CreacionDeQuizes());
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            cambiarColorBotones(button2.Name);
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            cambiarColorBotones(button1.Name);
+            //abrirFormHijo(new CrearPreguntasBaterias());
+        }
+
+        private void cambiarColorBotones(string nombreBoton) {
+            ControlCollection objetosDelFormulario = (ControlCollection)this.Controls;
+
+            foreach (Control c in objetosDelFormulario)
+            {
+                if (c is Panel)
+                {
+                    foreach (Control p in c.Controls)
+                    {
+                        if (p.GetType() == typeof(Button))
+                        {
+                            Button boton = (Button)p;
+                            if (p.Name == nombreBoton)
+                            {
+                                boton.BackColor = Color.MediumPurple;
+                                boton.ForeColor = Color.White;
+                            }
+                            else 
+                            {
+                                boton.BackColor = Color.SlateBlue;
+                                boton.ForeColor = Color.Black;
+                            }
+                        }
+                    }
+                }
+            }
+
         }
 
         private void panelContenedor_Paint(object sender, PaintEventArgs e)
@@ -57,9 +95,9 @@ namespace MyQuizifyGUI.Forms.InicioSesion
 
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
+        private void InicioInstructor_Load(object sender, EventArgs e)
         {
-            //abrirFormHijo(new CrearPreguntasBaterias());
+
         }
     }
 }
